74df0cfe02c42a23b56b60df24388d9cfa3b5aad
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2014-07-22 19:14:42 +0700

    Add accesstoken table

---
+++ db/6_add_accesstoken_table.sql
@@ -0,0 +1,15 @@
+SET client_encoding = 'UTF8';
+
+CREATE TABLE accesstoken (
+    token text,
+    "userId" text,
+    expired timestamp with time zone,
+    id integer NOT NULL,
+    "createdAt" timestamp with time zone,
+    "updatedAt" timestamp with time zone
+);
+
+ALTER TABLE ONLY accesstoken
+    ADD CONSTRAINT accesstoken_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY accesstoken
+    ADD CONSTRAINT accesstoken_token_key UNIQUE (token);
