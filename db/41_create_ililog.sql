8d9deb759968893c7c1a9e66314af059be2cc4e0
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2014-07-22 18:49:10 +0700

    Create ililog table

---
+++ db/41_create_ililog.sql
@@ -0,0 +1,17 @@
+SET client_encoding = 'UTF8';
+
+CREATE TABLE ililog IF NOT EXISTS (
+    source text,
+    date timestamp with time zone,
+    year integer,
+    week integer,
+    value double precision,
+    id integer NOT NULL,
+    "createdAt" timestamp with time zone,
+    "updatedAt" timestamp with time zone
+);
+
+ALTER TABLE ONLY ililog
+    ADD CONSTRAINT ililog_pkey PRIMARY KEY (id);
+
+CREATE INDEX ililog_source_date_year_week_idx ON ililog USING btree (source, date, year, week);
