86034c2b751f78162482e2022ed15480775e4cad
Nirut Khemasakchai <rutcreate@gmail.com>
2014-10-21 13:05:54 +0700

    Change the way to data from database via DBService.

--- api/services/EmailSubscriptionsService.js
+++ api/services/EmailSubscriptionsService.js
@@ -2,18 +2,22 @@ var when = require('when');
 
 module.exports = {
 
-    isSubscribed: function(client, user) {
-        return when.promise(function(resolve, reject) {
-            var statement = 'SELECT * FROM email_subscription WHERE "userId" = $1';
-            client.query(statement, [ user.id ], function(err, result) {
-                if (err) return reject(err);
-                resolve(result.rows.length > 0);
-            });
+    isSubscribed: function(user) {
+        return when.promise(function (resolve, reject) {
+            return DBService.select('email_subscription', '*', [
+                    { field: '"userId"', value: parseInt(user.id) }
+                ])
+                .then(function (result) {
+                    resolve(result.rows.length > 0);
+                })
+                .catch(function (err) {
+                    reject(err);
+                });
         });
     },
 
-    subscribe: function(client, user) {
-        return EmailSubscriptionsService.isSubscribed(client, user)
+    subscribe: function(user) {
+        return EmailSubscriptionsService.isSubscribed(user)
             .then(function(isSubscribed) {
                 if (!isSubscribed) {
                     return DBService.insert('email_subscription', [
@@ -38,7 +42,7 @@ module.exports = {
             });
     },
 
-    unsubscribe: function(client, user) {
+    unsubscribe: function(user) {
         return DBService.delete('email_subscription', [{
             field: '"userId"=$',
             value: user.id
